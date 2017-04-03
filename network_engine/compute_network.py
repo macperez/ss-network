52122ba07149319c82d823792594ce854d6f38c5
Christian Castillejo <automatas@institutoibt.com>
2017-04-03 14:16:03 +0200

    Primera versión de matriz de correlaciones

--- network_engine/compute_network.py
+++ network_engine/compute_network.py
@@ -2,8 +2,14 @@
 
 STEP = 2
 HISTORIAL_NUMBER_OF_ROWS = 14
-
+import pandas as pd
+import numpy as np
 def build (dt_close):
-    import ipdb; ipdb.set_trace()
-    
-    return None
+    todas=[]
+    for n in range(1,10,STEP):
+        R.append(np.corrcoef([dt_close.ix[n:n+HISTORIAL_NUMBER_OF_ROWS][col] for col in dt_close.columns]))
+
+
+    meanH=np.mean(triu(H))
+    stdH=np.std(triu(H))
+    return todas
