735c269f0979fac0f768fa740eee8703a8bef299
Manuel Antonio Castro Pérez <desarrollo@institutoibt.com>
2017-04-05 15:47:56 +0200

    Subida pruebas de arboles y pintado de grafos

--- network_engine/mst.py
+++ network_engine/mst.py
@@ -5,3 +5,21 @@ A collection of Minimal spawn tree algorithms implemented in Python
 @email: desarrollo@institutoibt.come
 
 """
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def main():
+    array = np.ones((4,4))
+    matrix = np.matrix([[1,1],[2,1]])
+    graph = nx.from_numpy_matrix(array)
+
+    tree = nx.minimum_spanning_tree(graph)
+    print (tree.edges())
+    nx.draw(graph)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
